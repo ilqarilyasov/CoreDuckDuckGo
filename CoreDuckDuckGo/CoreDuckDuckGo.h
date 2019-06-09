4e4b34868753c896d01467454ec62e01865070a9
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-09 17:23:52 -0500

    Initial Commit

---
+++ CoreDuckDuckGo/CoreDuckDuckGo.h
@@ -0,0 +1,19 @@
+//
+//  CoreDuckDuckGo.h
+//  CoreDuckDuckGo
+//
+//  Created by Ilgar Ilyasov on 6/9/19.
+//  Copyright © 2019 IIIyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDuckDuckGo.
+FOUNDATION_EXPORT double CoreDuckDuckGoVersionNumber;
+
+//! Project version string for CoreDuckDuckGo.
+FOUNDATION_EXPORT const unsigned char CoreDuckDuckGoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDuckDuckGo/PublicHeader.h>
+
+
---
+++ CoreDuckDuckGoTests/CoreDuckDuckGoTests.swift
@@ -0,0 +1,34 @@
+//
+//  CoreDuckDuckGoTests.swift
+//  CoreDuckDuckGoTests
+//
+//  Created by Ilgar Ilyasov on 6/9/19.
+//  Copyright © 2019 IIIyasov. All rights reserved.
+//
+
+import XCTest
+@testable import CoreDuckDuckGo
+
+class CoreDuckDuckGoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
